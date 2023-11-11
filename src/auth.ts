9d0c6370c6787b0c549c1dcdb007ff525c12d224
te19oishi <te19oishi@g.kumamoto-nct.ac.jp>
2023-11-11 10:48:06 +0900

    auth

--- src/auth.ts
+++ src/auth.ts
@@ -30,4 +30,41 @@ app.get('/api/auth', async (c) => {
   }
 })
 
+import { jwtVerify } from 'jose'
+
+// Google OAuthのリダイレクトURIエンドポイント
+app.get('/auth/google/callback', async (c) => {
+  const code = c.req.query('code')
+
+  if (!code) {
+    return c.json({ error: 'No code provided' }, 400)
+  }
+
+  // 認証コードをアクセストークンに交換
+  const tokenResponse = await fetch('https://oauth2.googleapis.com/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      code: code,
+      client_id: 'GOOGLE_CLIENT_ID',
+      client_secret: 'GOOGLE_CLIENT_SECRET',
+      redirect_uri: 'https://svapp-server.hinaharu-0014.workers.dev/auth/google/callback',
+      grant_type: 'authorization_code',
+    }),
+  })
+
+  const tokenData = await tokenResponse.json()
+  const idToken = tokenData.id_token
+
+  // IDトークン（JWT）のデコードと検証
+  try {
+    const { payload } = await jwtVerify(idToken, new TextEncoder().encode('YOUR_JWT_SECRET'))
+    return c.json({ user: payload })
+  } catch (err) {
+    return c.json({ error: 'Invalid token' }, 401)
+  }
+})
+
 export default app;
