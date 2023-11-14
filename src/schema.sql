39959f212e7d6beacd3c340af1bdb151668e059a
--global <--global>
2023-11-14 09:45:25 +0900

    Refactor schema.sql to use Users table instead of Employees table.

--- src/schema.sql
+++ src/schema.sql
@@ -1,32 +1,30 @@
-
-CREATE TABLE Employees (
-    employee_id INT PRIMARY KEY,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
+CREATE TABLE Users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
     email VARCHAR(255),
     role VARCHAR(100)
 );
 
 CREATE TABLE AttendanceRecords (
-    record_id INT PRIMARY KEY,
-    employee_id INT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
     date DATE,
     time_in TIME,
     time_out TIME,
-    FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
+    FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
 CREATE TABLE WorkTimeManagement (
-    record_id INT PRIMARY KEY,
-    employee_id INT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
     date DATE,
     worked_hours DECIMAL(5, 2),
     overtime_hours DECIMAL(5, 2),
     break_time DECIMAL(5, 2),
-    FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
+    FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
-CREATE INDEX idx_employee_id ON Employees(employee_id);
+CREATE INDEX idx_user_id ON Users(id);
 
-CREATE INDEX idx_worktime_employee_id ON WorkTimeManagement(employee_id);
+CREATE INDEX idx_worktime_user_id ON WorkTimeManagement(user_id);
 CREATE INDEX idx_worktime_date ON WorkTimeManagement(date);
