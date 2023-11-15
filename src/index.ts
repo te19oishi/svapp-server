a9ab522904add9fe351eb4c775aa087b806e5941
--global <--global>
2023-11-15 13:27:35 +0900

    Refactor API endpoints and add session management

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 
-interface Comment {
-	author: string;
-	body: string;
-}
-
 const app = new Hono();
 app.use('/api/*', cors());
 
@@ -13,7 +8,7 @@ app.get('/api/users/:email', async c => {
 	const { email } = c.req.param();
 	const { results } = await c.env.SVAPP_DB.prepare(
 		`
-    select * from users where email = ?
+    SELECT * FROM users WHERE email = ?
   `
 	)
 		.bind(email)
@@ -21,28 +16,57 @@ app.get('/api/users/:email', async c => {
 	return c.json(results);
 });
 
-app.post('/api/posts/:slug/comments', async c => {
-	const { slug } = c.req.param();
-	const { author, body } = await c.req.json<Comment>();
+interface UserInfo {
+	/**
+	 * The display name of the user.
+	 */
+	readonly displayName: string | null;
+	/**
+	 * The email of the user.
+	 */
+	readonly email: string | null;
+	/**
+	 * The phone number normalized based on the E.164 standard (e.g. +16505550101) for the
+	 * user.
+	 *
+	 * @remarks
+	 * This is null if the user has no phone credential linked to the account.
+	 */
+	readonly phoneNumber: string | null;
+	/**
+	 * The profile photo URL of the user.
+	 */
+	readonly photoURL: string | null;
+	/**
+	 * The provider used to authenticate the user.
+	 */
+	readonly providerId: string;
+	/**
+	 * The user's unique ID, scoped to the project.
+	 */
+	readonly uid: string;
+}
 
-	if (!author) return c.text('Missing author value for new comment');
-	if (!body) return c.text('Missing body value for new comment');
+// セッション情報を保存するためのエンドポイント
+app.post('/api/session', async c => {
+	const user = await c.req.json<UserInfo>();
+	const sessionId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+	await c.env.KV.set(sessionId, user);
+	return c.json({ sessionId });
+});
 
-	const { success } = await c.env.SVAPP_DB.prepare(
-		`
-    insert into comments (author, body, post_slug) values (?, ?, ?)
-  `
-	)
-		.bind(author, body, slug)
-		.run();
+// セッション情報を取得するためのエンドポイント
+app.get('/api/session/:sessionId', async c => {
+	const { sessionId } = c.req.param();
+	const user = await c.env.KV.get<UserInfo>(sessionId);
+	return c.json(user);
+});
 
-	if (success) {
-		c.status(201);
-		return c.text('Created');
-	} else {
-		c.status(500);
-		return c.text('Something went wrong');
-	}
+// セッション情報を削除するためのエンドポイント
+app.delete('/api/session/:sessionId', async c => {
+	const { sessionId } = c.req.param();
+	await c.env.KV.delete(sessionId);
+	return c.json({ status: 'ok' });
 });
 
 app.onError((err, c) => {
